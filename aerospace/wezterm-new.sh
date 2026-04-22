b14b8678a5bff09cb0460c5ce4289d9b5b25e630
hritikutekar <hritikdrocks@gmail.com>
2026-04-22 16:43:16 +0530

    feat: add nvim plugins and update aerospace/wezterm config

---
+++ aerospace/wezterm-new.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+open -na WezTerm
+sleep 0.3
+osascript -e 'tell application "WezTerm" to activate'
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,6 +14,17 @@ config.initial_rows = 28
 
 -- Hide tab bar
 config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_frame = {
+	border_left_width = "0.5cell",
+	border_right_width = "0.5cell",
+	border_bottom_height = "0.25cell",
+	border_top_height = "0.25cell",
+	border_left_color = "#45475C",
+	border_right_color = "#45475C",
+	border_bottom_color = "#45475C",
+	border_top_color = "#45475C",
+}
 
 -- or, changing the font size and color scheme.
 config.font_size = 14
@@ -21,10 +32,10 @@ config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.color_scheme = "Catppuccin Mocha"
 
 -- Enable transparency with black background
-config.window_background_opacity = 0.92
-config.macos_window_background_blur = 20
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
 config.colors = {
-	background = "#000000",
+	background = "#1E1E2E",
 }
 
 -- Finally, return the configuration to wezterm:
